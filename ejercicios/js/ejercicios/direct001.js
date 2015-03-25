62449dc4c94fab0ce289e8276589fb0ab47e4d82
juanjo <juanjo.lopez@biko2.com>
2015-03-25 10:51:17 +0100

    Directivas, priemros pasos

---
+++ ejercicios/js/ejercicios/direct001.js
@@ -0,0 +1,43 @@
+angular.module("angularApp", [])
+
+.controller('ctrlUno',  function($scope){
+    $scope.persona = {};
+    $scope.persona.nombre = "Fulano";
+    $scope.persona.apellidos = "Detal";
+})
+
+
+.controller('ctrlDos',  function($scope){
+    $scope.persona = {};
+    $scope.persona.nombre = "Ana";
+    $scope.persona.apellidos = "Pérez";
+})
+
+.directive("directv1", 
+    function(){
+        return {
+            /**
+             * AEC => La directiva se puede aplicar como Elemento, atributo, clase. 
+            */
+            //restrict:"AEC", 
+            //template: "Kaixo directiva!!"
+            templateUrl: "plantillas/e2form.html",
+            link: function(scope, element, attrs){ //Para modificar el DOM
+                //element.text("Hola");
+                //element.css("background-color", "red");
+                //console.log(attrs.misdatos); // <<-- se pondría <p directiv1 misdatos="loquesea"></p>
+                //console.log(scope);
+                //element.text(scope.nombre + " " + scope.apellidos);
+
+
+
+                element.empty();
+                var persona = scope[attrs["directv1"]];
+                var propiedadExpersion=attrs["propiedad"];
+                console.log(persona);
+                element.text(persona[propiedadExpersion]);
+            }
+        }
+    })
+
+;
